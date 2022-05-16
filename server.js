f092698a99f77343ef5b310ebc52f54f5d599dad
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-16 17:03:32 +0200

    set languages route & secure API

--- server.js
+++ server.js
@@ -2,10 +2,34 @@ const PORT = 8000;
 const axios = require("axios").default;
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-
+require("dotenv").config();
+console.log(process.env)
 const app = express();
 
 app.use(cors());
 
 app.listen(PORT, () => console.log("Server run on port " + PORT));
+
+app.get("/languages", async (req, res) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    },
+  };
+
+  try {
+    const response = await axios(
+      "https://google-translate20.p.rapidapi.com/languages",
+      options
+    );
+    const arrayOfData = Object.keys(response.data.data).map(
+      (key) => response.data.data[key]
+    );
+    res.status(200).json(arrayOfData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
+});
