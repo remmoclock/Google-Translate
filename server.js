a755d193a228a9492eba94cbd179c1f8fe45aa27
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-16 16:41:56 +0200

    make it better

--- server.js
+++ server.js
@@ -10,15 +10,19 @@ app.use(cors());
 
 app.listen(PORT, () => console.log("Server run on port " + PORT));
 
-app.use(
-  express.static(path.join(__dirname, "/client/build"))
-);
+// app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "/client/build", "index.html")
-  );
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+// });
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.get("/languages", async (req, res) => {
   const options = {
